04a9e8b3940b74bf50cdce949b4a87c2a629aacf
jforaker <jake@chalkable.com>
2016-09-21 09:08:23 -0400

    bundle

--- demo/bundle.js
+++ demo/bundle.js
@@ -21954,6 +21954,8 @@
 	  value: true
 	});
 	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
 	var _react = __webpack_require__(2);
@@ -22030,7 +22032,7 @@
 	    value: function render() {
 	      return _react2.default.createElement(
 	        'div',
-	        { className: (0, _classnames2.default)('accordion-node', this.props.className), style: defaultStyles },
+	        { className: (0, _classnames2.default)('accordion-node', this.props.className), style: _extends({}, defaultStyles, this.props.style) },
 	        this.renderNodeItems()
 	      );
 	    }
