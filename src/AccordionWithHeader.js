8e035157ae286cd891278c5e616a9783cc1bdaea
Yankarlo <yrios@vovsolutions.com>
2018-07-05 10:27:43 -0500

    add new prop

--- src/AccordionWithHeader.js
+++ src/AccordionWithHeader.js
@@ -5,7 +5,8 @@ import classNames from 'classnames';
 
 const defaultProps = {
   multipleOkay: false,
-  firstOpen: false
+  firstOpen: false,
+  isOpen: undefined
 };
 
 export default class AccordionWithHeader extends Component {
@@ -38,6 +39,7 @@ export default class AccordionWithHeader extends Component {
   mountingProps(props) {
     let active = [];
     if (props.firstOpen) active.push(0);
+    if (props.isOpen !== undefined) active.push(props.isOpen);
     this.setState({ active: active });
   }
 
@@ -95,6 +97,7 @@ export default class AccordionWithHeader extends Component {
 AccordionWithHeader.propTypes = {
   firstOpen: PropTypes.bool,
   multipleOkay: PropTypes.bool,
-  actionCallback: PropTypes.func
+  actionCallback: PropTypes.func,
+  isOpen: PropTypes.number
 };
 AccordionWithHeader.defaultProps = defaultProps;
